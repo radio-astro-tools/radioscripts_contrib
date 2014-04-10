d00acf91c5ce39fa0c12a4376338cc8e204ec589
Adam Ginsburg (keflavich) <keflavich@gmail.com>
2014-04-10 16:04:00 -0400

    added documentation

--- python/find_lines_in_header.py
+++ python/find_lines_in_header.py
@@ -5,6 +5,24 @@ from astropy import units as u
 import numpy as np
 
 def find_lines_in_header(header, return_pixels=False, **kwargs):
+    """
+    Create a dictionary of line name: line position (in pixel units or
+    frequency) for later extraction
+
+    Parameters
+    ----------
+    header: astropy.io.fits.Header
+    return_pixels: bool
+        Return the line dictionary with pixel units in the header?
+        Otherwises, returns frequency (in GHz)
+
+    Examples
+    --------
+    >>> from astropy.io import fits
+    >>> header = fits.getheader('cubefile.fits')
+    >>> linedict = find_lines_in_header(header, energy_max=50,
+    ...                                 energy_type='eu_k')
+    """
 
     naxis3 = header['NAXIS3']
     wcs = WCS(header)
